8061ad8c638df5d8e9ea048a5204b7a605f96bcf
Shadman <shadmansaleh3@gmail.com>
2021-01-31 13:27:55 +0600

    Added solarized theme (#63)
    * Added solarized theme

--- lua/lualine/themes/solarized.lua
+++ lua/lualine/themes/solarized.lua
@@ -6,5 +6,9 @@
 -- =============================================================================
 
 local background = vim.o.background
+<<<<<<< HEAD
+=======
+print(background)
+>>>>>>> ed70e16 (Added solarized theme (#63))
 
 return require("lualine.themes.solarized_"..background)
